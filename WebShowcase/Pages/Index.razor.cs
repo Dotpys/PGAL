7d12a413ac1b73269898b7f51391792f4f29c485
Julien Russo <julien.russo@outlook.com>
2023-07-05 05:18:28 +0200

    Added a simple blazor project to showcase all the library functionalities.

---
+++ WebShowcase/Pages/Index.razor.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+using System.Runtime.Versioning;
+
+namespace WebShowcase.Pages;
+
+[SupportedOSPlatform("browser")]
+public partial class Index
+{
+	[JSImport("loadImage", "Index")]
+	internal static partial void LoadImage(byte[] data);
+}
