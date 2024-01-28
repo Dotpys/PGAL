5551c7c4f6f03796e641a9157544362ebdd75766
Julien Russo <julien.russo@outlook.com>
2024-01-28 12:59:02 +0100

    CSS fixes + delaunay test

---
+++ WebShowcase/Pages/VoronoiDiagramDelaunay.razor.cs
@@ -0,0 +1,17 @@
+﻿using System.Numerics;
+using System.Runtime.Versioning;
+
+namespace WebShowcase.Pages;
+
+[SupportedOSPlatform("browser")]
+public partial class VoronoiDiagram
+{
+}
+
+public static class ExtensionMethods
+{
+	public static IEnumerable<IEnumerable<T>> DifferentCombinations<T>(this IEnumerable<T> elements, int k)
+	{
+		return k == 0 ? new[] { Array.Empty<T>() } : elements.SelectMany((e, i) => elements.Skip(i + 1).DifferentCombinations(k - 1).Select(c => (new[] { e }).Concat(c)));
+	}
+}
---
+++ WebShowcase/Pages/VoronoiDiagramFortune.razor.cs
@@ -0,0 +1,59 @@
+﻿using System.Numerics;
+using System.Runtime.Versioning;
+
+namespace WebShowcase.Pages;
+
+[SupportedOSPlatform("browser")]
+public partial class VoronoiDiagram
+{
+}
+
+public class Voronoi
+{
+	public void GenerateDiagram(IEnumerable<(float x, float y)> sites)
+	{
+		PriorityQueue<VoronoiEvent, float> eventQueue = new();
+
+		foreach (var site in sites)
+		{
+			eventQueue.Enqueue(new VoronoiSiteEvent(), site.y);
+		}
+
+		while(eventQueue.TryDequeue(out VoronoiEvent? e, out float y))
+		{
+			if (e is VoronoiSiteEvent)
+			{
+				//InsertParabola(point)
+				/*
+					if root = null
+						root = new Parabola(p)
+						return
+
+					if root.isLeaf AND root.site.y - p.y < 1
+						// Handle edge case
+
+					parabola = GetParabolaByX(point.x)
+				 */
+			}
+			else
+			{
+				//RemoveParabola(event);
+			}
+		}
+	}
+}
+
+public abstract class VoronoiEvent
+{
+
+}
+
+public sealed class VoronoiSiteEvent : VoronoiEvent
+{
+
+}
+
+public sealed class VoronoiVertexEvent : VoronoiEvent
+{
+
+}
